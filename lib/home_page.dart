d24bc400703e8771adfd1060c40874ec4648829f
NanuSai <gnanasai.n18@iiits.in>
2020-01-02 23:34:30 +0530

    Added Icons and Search bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,13 +6,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  Icon customIcon = Icon(Icons.search);
+  Widget customTitle = Text("Main Page");
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Route"),
+        title: customTitle,
         backgroundColor: Colors.deepOrange,
-        actions: <Widget>[],
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            //Leading are the things before title
+            print("Clicked");
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: customIcon,
+            onPressed: () {
+              print("Searching");
+              setState(() {
+                if (this.customIcon.icon == Icons.search) {
+                  this.customIcon = Icon(Icons.cancel);
+                  this.customTitle = TextField(
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+
+                    ),
+                    textInputAction: TextInputAction.go,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search term..."
+                    ),
+                  );
+                }
+                else {
+                  this.customIcon = Icon(Icons.search);
+                  this.customTitle = Text("Main Page");
+                }
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+              print("vertical");
+            },
+          ),
+        ],
+        elevation: 14,
+
+        titleSpacing: 12,
       ),
       body: Center(
         child: RaisedButton(
