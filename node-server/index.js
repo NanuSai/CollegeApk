c7f293be293ab80ee0d8e9d180ef2a4fbd234adf
Ashutosh Chauhan <masterashu@live.in>
2020-01-04 17:31:55 +0530

    fixing nodejsserver

--- node-server/index.js
+++ node-server/index.js
@@ -9,54 +9,54 @@ const { parse } = require('querystring');
 
 app.use(bodyParser.json());
 
-function insertDocument(username,password){
+function insertDocument(username, password) {
 
-MongoClient.connect(SERVER_URL,(err,db)=>{
+    MongoClient.connect(SERVER_URL, (err, db) => {
 
         if (err) throw err;
         console.log("Connected to database! ");
         var dbo = db.db("Users");
-        var myobj = {username:username,password:password};
-        dbo.collection("login_info").insertOne(myobj,(err,res)=>{
+        var myobj = { username: username, password: password };
+        dbo.collection("login_info").insertOne(myobj, (err, res) => {
             if (err) throw err;
             console.log("One document inserted!");
-            db.close();  
-            
+            db.close();
+
         })
-})
+    })
 }
 
-function isDocument(username,password){
+function isDocument(username, password) {
 
-        MongoClient.connect(SERVER_URL,(err,db) => {
+    MongoClient.connect(SERVER_URL, (err, db) => {
 
+        if (err) throw err;
+        console.log("Connected to database!");
+        var dbo = db.db("Users");
+        dbo.collection("login_info").findOne({}, { projection: { username: username, password: password } }, (err, res) => {
             if (err) throw err;
-            console.log("Connected to database!");
-            var dbo = db.db("Users");
-            dbo.collection("login_info").findOne({},{projection : {username : username,password:password}},(err,res)=>{
-                if(err) throw err;
-                console.log("Document found!");
-                return true;
-            }
+            console.log("Document found!");
+            return true;
+        }
         )
-})
+    })
 }
 
 
 
-app.get('/',(req,res)=>{
-   res.send("Welcome to the Oasis! Hosted by Nanubala Gnana Sai");
-   
+app.get('/', (req, res) => {
+    res.send("Welcome to the Oasis! Hosted by Nanubala Gnana Sai");
+
 });
 
-app.post('/login',(req,res)=>{
-    
+app.post('/login', (req, res) => {
+    console.log(req.body)
     var username = req.body.username;
     var password = req.body.password;
     console.log(username);
     console.log(password);
-    insertDocument(username,password);
-
+    insertDocument(username, password);
+    res.send("Login done");
 });
 
 
